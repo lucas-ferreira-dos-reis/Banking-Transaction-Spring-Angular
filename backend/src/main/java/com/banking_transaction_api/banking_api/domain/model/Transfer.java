dd231a964fd8f89d1ccf35cd3a8f558738eedc54
Lucas Ferreira dos Reis <lucas.reis@adisra.com>
2026-08-04 23:41:04 -0300

    feat: create Transfer entity

---
+++ backend/src/main/java/com/banking_transaction_api/banking_api/domain/model/Transfer.java
@@ -0,0 +1,41 @@
+package com.banking_transaction_api.banking_api.domain.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_transfer")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transfer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 10)
+    private String originAccount;
+
+    @Column(nullable = false, length = 10)
+    private String targetAccount;
+
+    @Column(nullable = false)
+    private BigDecimal value;
+
+    @Column(nullable = false)
+    private BigDecimal tax;
+
+    @Column(nullable = false)
+    private LocalDate creationDate;
+
+    @Column(nullable = false)
+    private LocalDate scheduledDate;
+}
